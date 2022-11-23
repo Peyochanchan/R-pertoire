9517a65f25035ddbca4b36aff86d021f802de19c
Pierre Cherbero <cameleon24@outlook.fr>
2022-11-23 15:26:24 +0100

    homepage header

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home 
+  skip_before_action :authenticate_user!, only: %i[home poldeconf cgu]
 
   def home
+    @lists = policy_scope(List)
+    @songs = policy_scope(Song)
   end
 end
